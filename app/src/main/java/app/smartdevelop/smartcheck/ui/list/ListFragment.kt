f95e6b8b5c9115389323ff171381af07ddd257ce
EGL-PC <93424830+eglez7@users.noreply.github.com>
2023-05-02 20:39:49 +0200

    primer cuadro de dialogo extraido

--- app/src/main/java/app/smartdevelop/smartcheck/ui/list/ListFragment.kt
+++ app/src/main/java/app/smartdevelop/smartcheck/ui/list/ListFragment.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
-import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import app.smartdevelop.smartcheck.R
@@ -72,9 +70,6 @@ class ListFragment :  Fragment() {
             if (result != "") insertListDB(result)
         }
     }
-
-
-
 
     private fun insertListDB(checklistName: String) {
         CoroutineScope(Dispatchers.IO).launch {
